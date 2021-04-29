3f3cb57a15524ba6023e47a450360b37efacd55c
Kristof Kovacs <23499974+kristofk01@users.noreply.github.com>
2021-04-29 16:29:33 +0800

    Another test...

--- MyProject.java
+++ MyProject.java
@@ -3,36 +3,36 @@
 import java.util.Array;
 
 public class MyProject implements Project {
-    public boolean allDevicesConnected(int[][] adjlist) {
-        //will need a visited array
-        //go through adjList[0] and mark each node as visited
-        //make bfs -> look at SearchImp
-        //or prims algorithm to make min spanning tree?
-        if (!visited){
-        for (int i = 0; i < size; i++){
-            getArray[i];
-            size2 = secondArray.size
-         for (int j = 0; j < size2; j++){
-          adjList[i][j] = visited;   
-         }
+  public boolean allDevicesConnected(int[][] adjlist) {
+    //will need a visited array
+    //go through adjList[0] and mark each node as visited
+    //make bfs -> look at SearchImp
+    //or prims algorithm to make min spanning tree?
+    if (!visited) {
+      for (int i = 0; i < size; i++) {
+        getArray[i];
+        size2 = secondArray.length
+        for (int j = 0; j < size2; j++) {
+          adjList[i][j] = visited;
         }
-       } 
-        if(allVisited = true)
-        return true;
+      }
     }
+    if(allVisited = true)
+    return true;
+  }
 
-    public int numPaths(int[][] adjlist, int src, int dst) {
-        // TODO
-        return 0;
-    }
+  public int numPaths(int[][] adjlist, int src, int dst) {
+    // TODO
+    return 0;
+  }
 
-    public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][] queries) {
-        // TODO
-        return null;
-    }
+  public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][] queries) {
+    // TODO
+    return null;
+  }
 
-    public int maxDownloadSpeed(int[][] adjlist, int[][] speeds, int src, int dst) {
-        // TODO
-        return 0;
-    }
+  public int maxDownloadSpeed(int[][] adjlist, int[][] speeds, int src, int dst) {
+    // TODO
+    return 0;
+  }
 }
