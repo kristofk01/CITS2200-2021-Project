3ec9dc5a852199c858dbce989948e20a1b109f15
Texas-Alex <82863901+Texas-Alex@users.noreply.github.com>
2021-04-30 15:37:21 +0800

    Update MyProject.java
    removed unconcise code and added a comment

--- MyProject.java
+++ MyProject.java
@@ -1,28 +1,21 @@
 // Alexandria Bennett(22969368), Kristof Kovacs (22869854)
 
 public class MyProject implements Project {
+  /** checks whether all of the devices in the network are connected.
+   *@param adjList is the adjaceny matrix of the graph being checked.
+   */
+  
   public boolean allDevicesConnected(int[][] adjlist) {
     
     int size = adjlist.length;
     boolean[] connected = new boolean[size];
 
-    connected[0] = true;    
+    connected[0] = true;    //initial node
    
     for (int i = 0; i < size; i++) {
       for (int j = 0; j < adjlist[i].length; j++) {
         connected[adjlist[i][j]] = true;
-        
-    connected[0] = true;
-    
-   
-    for (int i = 0; i < size; i++) {
-        int size2 = adjList[i].length;
-        for (int j = 0; j < size2; j++) {
-          int check = adjList[i][j];
-          connected[check] = true;
-        }
-
-      }
+       }
     }
 
     for(boolean x : connected) {
