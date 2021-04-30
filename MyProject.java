68752d0a403f8a8f9d6bb167caaa97ad3b4201e1
Kristof Kovacs <23499974+kristofk01@users.noreply.github.com>
2021-04-30 17:04:43 +0800

    BFS solution for numPaths(), passes all but one test (two-path connected graph)

--- MyProject.java
+++ MyProject.java
@@ -1,12 +1,20 @@
 // Alexandria Bennett(22969368), Kristof Kovacs (22869854)
 
+import java.util.*;
+
 public class MyProject implements Project {
+  private PriorityQueue<Integer> queue;
+
+  public MyProject () {
+    queue = new PriorityQueue<>();
+  }
+
   /**
    * checks whether all of the devices in the network are connected.
    * @param adjList is the adjaceny matrix of the graph being checked.
    */
   public boolean allDevicesConnected(int[][] adjlist) {
-    // Note: This is O(n^2), our goal is O(n)    
+    // Note: This is O(n^2), our goal is O(n)
     int size = adjlist.length;
     boolean[] connected = new boolean[size];
 
@@ -18,15 +26,32 @@ public class MyProject implements Project {
       }
     }
 
-    for(boolean x : connected) {
-      if(!x) return false;
+    for (boolean x : connected) {
+      if (!x) return false;
     }
     return true;
   }
 
   public int numPaths(int[][] adjlist, int src, int dst) {
-    // TODO
-    return 0;
+    int count = 0;
+    boolean[] visited = new boolean[adjlist.length];
+
+    queue.add(src);
+    while (!queue.isEmpty()) {
+      int current = queue.remove();
+      if (current == dst) {
+        count++;
+      }
+
+      for (int i = 0; i < adjlist[current].length; i++) {
+        if (!visited[i]) {
+          queue.add(adjlist[current][i]);
+          visited[i] = true;
+        }
+      }
+    }
+
+    return count;
   }
 
   public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][] queries) {
