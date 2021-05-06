dd997d03ca505a1a4306884a0adad4f4fd583c59
Texas-Alex <82863901+Texas-Alex@users.noreply.github.com>
2021-05-06 17:57:50 +0800

    Update MyProject.java
    wrote what we'll probably need to do for maxDownloadSpeed and closestInSubnet. the subnet one i think will be hardest to  figue out due to the ip address but i tried to write it out. minor change to numPaths but untested (might not do anything tbh)

--- MyProject.java
+++ MyProject.java
@@ -53,7 +53,7 @@ public class MyProject implements Project {
         count++;
 
       for (int vertex : adjlist[current]) {
-        if (!visited[vertex]) {
+        if (visited[vertex]) continue{
           queue.add(vertex);
           visited[vertex] = true;
         }
@@ -64,12 +64,27 @@ public class MyProject implements Project {
   }
 
   public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][] queries) {
-    // TODO
+    //Dijkstra's algorithm with binary heap has the complexity we need (wikipedia lmao)
+    /*
+     *addrs[i][] = an ip adress of 4 vals from 1-255 for device i
+     *queries[i][] = ip address prefix? eg for {198, 34, 1, 1} it would be {198, 34} but not {198, 34, 2}
+     *probably going to want to take the address from the source and go to eaach node 1 index at a time
+     *eg) 
+     *get.addrs[src]. for each query, is queries[i][0] = addrs[src][0] if so move on to queries[i][1] etc
+     *i think the queries arrays will not be the same length as addrs so that will need to be checked. 
+     *returning the number of hops to get to a subnet (from queries)
+     */
     return null;
   }
 
   public int maxDownloadSpeed(int[][] adjlist, int[][] speeds, int src, int dst) {
-    // TODO
+   //Bellman Ford or Flyod Warshall depending on the complexity (VE vs V^3)
+    /* speeds[i][j] relates to the device at adjList[i][j]
+     * instead of smallest path, we need biggest path eg MAX speed
+     * can travel multiple paths at once and can be asymetric down a link -> check both ways?
+     *if (src == dst){return -1}
+     *dijkstra again (or similar) to grab the max total speed from the source to destination.
+     */
     return 0;
   }
 }
