fe949d9866ab6cfc68cdd0be61e24b53addca670
Kristof Kovacs <23499974+kristofk01@users.noreply.github.com>
2021-05-21 21:19:06 +0800

    Finally finished????

--- MyProject.java
+++ MyProject.java
@@ -15,7 +15,8 @@ public class MyProject implements Project {
    */
   public boolean allDevicesConnected(int[][] adjlist) {
     boolean[]visited = bfs(adjlist);
-    //transpose the array to check if all nodes connect to 0.  
+
+    // transpose the array to check if all nodes connect to 0.
     boolean[] visited0 = bfs(transpose(adjlist));
 
     for (int i = 0; i < adjlist.length; i++)
@@ -98,44 +99,27 @@ public class MyProject implements Project {
   public int numPaths(int[][] adjlist, int src, int dst) {
     if (src == dst) return 1;
 
-    Stack<Integer> stack = new Stack<>();
+    Queue<Integer> queue = new LinkedList<>();
     int deviceCount = adjlist.length;
     boolean[] visited = new boolean[deviceCount];
-    int[] parent = new int[deviceCount];
+    int count = 0;
 
-    stack.push(src);
+    queue.add(src);
     visited[src] = true;
 
-    int count = 0;
-    while (!stack.empty()) {
-      int current = stack.pop();
-      visited[current] = true;
-
-      if (parent[current] == dst) {
-        visited[dst] = true;
-      }
-      else {
-        visited[dst] = false;
-      }
-
-      if (current == dst) {
-        count++;
-      }
-
+    while (!queue.isEmpty()) {
+      int current = queue.remove();
       for (int vertex : adjlist[current]) {
-        /*
-        if (vertex == dst && visited[parent[vertex]]) {
-          System.out.println(current + ", " + vertex + ", " + parent[vertex]);
+        if (vertex == dst) {
           count++;
         }
-        */
-        if (vertex != current && !visited[vertex] && !stack.contains(vertex)) {
-          parent[vertex] = current;
-          stack.push(vertex);
+        else if (!visited[vertex]) {
+          queue.add(vertex);
+          visited[vertex] = true;
         }
       }
     }
-    
+
     return count;
   }
 
@@ -154,30 +138,31 @@ public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][
     int[] hopsByQuery = new int[queries.length];
     Arrays.fill(hopsByQuery, Integer.MAX_VALUE);
 
-    int[] distances = bfsDistances(adjlist,src); //shortest path to each node from the source
-    
-    for (int i = 0; i < queries.length; i++) {//for each query...
+    // Shortest path to each node from the source
+    int[] distances = bfsDistances(adjlist,src);
+
+    for (int i = 0; i < queries.length; i++) {
       short[] subnet = queries[i];
- 
+
       int min_distance = Integer.MAX_VALUE;
 
       for (int j = 0; j < deviceCount; j++) {
         short[] device_address = addrs[j];
-        boolean not_in_subnet = false; //point not in subnet
+        boolean isInSubnet = true;
         for (int k = 0; k < subnet.length; k++) {
-          // If not in the subnet
+          // If the device is not in the subnet
           if (subnet[k] != device_address[k]) {
-            not_in_subnet = true;
+            isInSubnet = false;
           }
         }
 
-        if(!not_in_subnet && distances[j] < min_distance){//if device in subnet and its distance < current min distance
+        // If the device is in the subnet and it's distance < current min distance
+        if(isInSubnet && distances[j] < min_distance) {
           min_distance = distances[j];
-
         }
       }
       hopsByQuery[i] = min_distance;
-    }    
+    }
     
     return hopsByQuery;
   }
@@ -217,7 +202,6 @@ public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][
    */
   public int maxDownloadSpeed(int[][] adjlist, int[][] speeds, int src, int dst) {
     // Note: "flow" and "speed" are interchangable...
-
     if (src == dst) return -1;
 
     int deviceCount = adjlist.length;
@@ -230,6 +214,8 @@ public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][
       }
     }
 
+    // Contains current best possible speeds - will be augmented until
+    // we don't reach the destination through BFS.
     int[][] flow = new int[deviceCount][deviceCount];
 
     boolean canFlow = true;
@@ -249,8 +235,8 @@ public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][
         }
 
         // For every adjacent device, check if has not been visited and
-        // that the current flow rate for this device is less than the
-        // max flow rate.
+        // that the flow rate between ``current`` and its adjacent devices ``i``
+        // is less than the max flow rate through that same edge.
         for (int i = 0; i < deviceCount; i++) {
           if (!visited[i] && speedsMatrix[current][i] > flow[current][i]) {
             queue.add(i);
@@ -264,14 +250,14 @@ public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][
       if (!canFlow)
         break;
       
-      // Find new minimum speed by following the path the BFS took from the source
-      // to the destination.
+      // Find new minimum speed by following the path the BFS took (reversed)
+      // i.e. from the destination to the source (excluding the source)
       int minSpeed = speedsMatrix[parent[dst]][dst] - flow[parent[dst]][dst];
       for (int i = dst; i != src; i = parent[i]) {
         minSpeed = Math.min(minSpeed, (speedsMatrix[parent[i]][i] - flow[parent[i]][i]));
       }
 
-      // Update flow values - following the same path (backtracking).
+      // Update flow values - following the same path as above
       for (int i = dst; i != src; i = parent[i]) {
         flow[parent[i]][i] += minSpeed;
         flow[i][parent[i]] -= minSpeed;
